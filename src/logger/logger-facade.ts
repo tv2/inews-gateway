e3f952f28f7c118aabb609cc80223953ffded95a
Anders Frederik Jørgensen <afjo@tv2.dk>
2024-07-09 15:42:03 +0200

    test: Added tests for IngestEventServer.

--- src/logger/logger-facade.ts
+++ src/logger/logger-facade.ts
@@ -1,8 +1,13 @@
 import { Logger } from './logger'
 import { ConsoleLogger } from './console-logger'
+import { DummyLogger } from './dummy-logger'
 
 export class LoggerFacade {
   public static createLogger(): Logger {
     return ConsoleLogger.getInstance()
+  }
+
+  public static createDummyLogger(): Logger {
+    return new DummyLogger()
   }
 }

d7f52938e5be1733bd81337946c24213631ab1b8
Anders Frederik Jørgensen <afjo@tv2.dk>
2024-07-16 09:37:59 +0200

    chore: Adds connect and disconnect methods to InewsClient interface.

--- src/business-logic/interfaces/inews-client.ts
+++ src/business-logic/interfaces/inews-client.ts
@@ -1,6 +1,8 @@
 import { StoryMetadata } from '../value-objects/story-metadata'
 
 export interface InewsClient {
+  connect(): Promise<void>
   getStoryMetadataForQueue(queueId: string): Promise<readonly StoryMetadata[]>
   getStory(queueId: string, storyId: string): Promise<unknown>
+  disconnect(): Promise<void>
 }
